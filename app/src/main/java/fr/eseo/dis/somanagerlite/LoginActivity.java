1d29538c01ed90adbd4a84cfef9a1d7310e0d185
quentin <emilien.mamalet@gmail.com>
2018-10-09 15:31:12 +0200

    Update

--- app/src/main/java/fr/eseo/dis/somanagerlite/LoginActivity.java
+++ app/src/main/java/fr/eseo/dis/somanagerlite/LoginActivity.java
@@ -341,7 +341,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
             if (success) {
                 finish();
-                Intent intent = new Intent(getBaseContext(), OptionActivity.class);
+                Intent intent = new Intent(getBaseContext(), MenuActivity.class);
                 startActivity(intent);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
--- app/src/main/java/fr/eseo/dis/somanagerlite/MenuProjectActivity.java
+++ app/src/main/java/fr/eseo/dis/somanagerlite/MenuProjectActivity.java
@@ -4,29 +4,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
 
 
-public class OptionPosterActivity extends AppCompatActivity {
+public class MenuProjectActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.option_poster_card_layout);
-        RecyclerView recycler = (RecyclerView)findViewById(R.id.optionList);
-        recycler.setHasFixedSize(true);
+        setContentView(R.layout.activity_menu_poster);
         LinearLayoutManager llm = new LinearLayoutManager(this);
         llm.setOrientation(LinearLayoutManager.VERTICAL);
-        recycler.setLayoutManager(llm);
         loadOptionSelectedData();
     }
 
     private void loadOptionSelectedData(){
 
     }
-
-
-
-
 }
