fe8576bfab463996bb62b96ddfffbdcdd2438b75
quentin <emilien.mamalet@gmail.com>
2018-10-13 15:24:35 +0200

    Update

--- app/src/main/java/fr/eseo/dis/somanagerlite/MenuPosterActivity.java
+++ app/src/main/java/fr/eseo/dis/somanagerlite/MenuPosterActivity.java
@@ -1,15 +1,10 @@
 package fr.eseo.dis.somanagerlite;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import fr.eseo.dis.somanagerlite.data.Poster;
 import fr.eseo.dis.somanagerlite.data.adapters.MenuPosterAdapter;
 import fr.eseo.dis.somanagerlite.data.source.DummyData;
 
